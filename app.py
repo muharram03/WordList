08304843a7642fe64ca87299ac3f11548f40fb15
Muharram Mahdafiqia <muharram.200150126@mhs.unimal.ac.id>
2023-11-09 15:06:03 +0700

    flask setup for deployment

--- app.py
+++ app.py
@@ -1,18 +1,25 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
 from flask import Flask, request, render_template, redirect, url_for, jsonify
 from pymongo import MongoClient
 import requests
 from datetime import datetime
 from bson import ObjectId
 
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+MONGODB_URI = os.environ.get("MONGODB_URI")
+DB_NAME =  os.environ.get("DB_NAME")
+
+client = MongoClient(MONGODB_URI)
+
+db = client[DB_NAME]
 
 app = Flask(__name__)
 
-
-password = '200150126'
-cxn_str = f'mongodb+srv://muharram200150126:{password}@cluster0.qxfmag2.mongodb.net/?retryWrites=true&w=majority'
-client = MongoClient(cxn_str)
-
-db = client.dbsparta_plus_week2
 
 @app.route('/')
 def main():
